c75004577c58885d0b4f5ca7e5869b0e0b1244c5
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-01-17 15:30:21 +0600

    Rootlayout section Design done

---
+++ src/components/LightRays.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LightRays() {
+    return (
+        <div>LightRays</div>
+    )
+}
+
+export default LightRays
--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
-import Logo from '../shared/Logo'
+import React, { useEffect, useState } from 'react'
+import { Outlet, useNavigation } from 'react-router-dom';
+import Loading from '../components/Loading';
+import LightRays from '../components/LightRays';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
 
 function RootLayout() {
+    const navigation = useNavigation();
+    const isLoading = navigation.state === "loading";
+    const [initialLoading, setInitialLoading] = useState(true)
+    const [loadingComplete, setLoadingComplete] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoadingComplete(true); // Trigger curtain animation
+
+            // Remove loader after animation completes
+            setTimeout(() => {
+                setInitialLoading(false);
+            }, 700); // Wait for 1s curtain animation
+        }, 1500);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (initialLoading) {
+        return <Loading isComplete={loadingComplete} />;
+    }
+
     return (
-        <div>
-            <Logo />
-        </div>
+        <>
+            {isLoading && <Loading isComplete={false} />}
+            <div className='bg-linear-to-r from-[#0a0a0fec] to-gray-800 min-h-screen text-accent relative'>
+                <div className='absolute top-0 left-0 w-full h-screen z-0 pointer-events-none'>
+                    <LightRays
+                        raysOrigin="top-center"
+                        raysColor="#dcd6ff"
+                        raysSpeed={1.2}
+                        lightSpread={0.9}
+                        rayLength={1.5}
+                        followMouse={true}
+                        mouseInfluence={0.15}
+                        noiseAmount={0.05}
+                        distortion={0.03}
+                    />
+                </div>
+
+                {/* Content Layer */}
+                <div className='relative z-10'>
+                    <header className='sticky backdrop-blur-xl top-0 z-500'>
+                        <Navbar />
+                    </header>
+                    <main className='min-h-[calc(100vh-65px)] max-w-7xl mx-auto my-6 sm:my-8 md:my-10 lg:my-12 px-5 sm:px-8 xl:px-0 text-sm md:text-base overflow-hidden'>
+                        <Outlet />
+                    </main>
+                    <Footer />
+                </div>
+            </div>
+        </>
     )
 }
 
